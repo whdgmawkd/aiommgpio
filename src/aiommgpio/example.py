979608d2899f51480ddb9220dfa284a0c5578cf6
parkjongheum <whdgmawkd@naver.com>
2022-12-12 21:30:18 +0900

    exampl: add wait for edge example

--- src/aiommgpio/example.py
+++ src/aiommgpio/example.py
@@ -6,6 +6,7 @@ async def main():
     mmio = RPiMMIO()
     pwm = await mmio.get_pwm(18, PWM_MODE.HARDWARE)
     gpio = await mmio.get_gpio(12, GPIO_MODE.OUTPUT)
+    gpio_input = await mmio.get_gpio(13, GPIO_MODE.INPUT)
     await pwm.set_frequency(25000)  # 25KHz frequency
     await pwm.set_duty(20000)  # 20000 nanoseconds duty
     await pwm.start()
@@ -15,6 +16,7 @@ async def main():
     await gpio.write(True)
     await asyncio.sleep(5)
     await gpio.write(False)
+    print(f'GPIO Wait for edge: {await gpio_input.wait_for_edge()}')
     await pwm.cleanup()
     await gpio.cleanup()
 
